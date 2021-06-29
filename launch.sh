1ab9d7ddea4586b5a5aad734556b27182963cffd
Lara <lara.bondageclub@gmail.com>
2021-06-29 20:46:58 +0100

    Assume in virtual environment

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./venv/bin/python bot.py
+python bot.py
 while [ $? -eq 11 ]; do
     echo "Server requested restart.  Respawning.." >&2
     git pull
     sleep 1
-    ./venv/bin/python bot.py
+    python bot.py
 done
